5ae0007ad6138964db0d9b57b53f8245f835ffd0
Olga Babushkina <babushkinaoa@mail.ru>
2022-12-25 21:45:29 +0700

    variables for solution added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите слова через запятую без пробелов");
+string? userInput = Console.ReadLine();
+char divider = ',';
+int count = 1;
+int quantity = 0;
+int minSymbolNumber = 3;
+int l = 0;
+
