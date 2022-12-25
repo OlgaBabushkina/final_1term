49547bcc1e010cbf1c60856c85026a61fa3e6b91
Olga Babushkina <babushkinaoa@mail.ru>
2022-12-25 21:50:57 +0700

    cycle for counting input words&defining size of user array added

--- Program.cs
+++ Program.cs
@@ -6,3 +6,10 @@ int quantity = 0;
 int minSymbolNumber = 3;
 int l = 0;
 
+for (int i = 0; i < userInput.Length; i++)//цикл определяет количество слов введенных пользователем и размер массива
+{
+    if (userInput[i] == divider)
+    {
+        count++;
+    }
+}
